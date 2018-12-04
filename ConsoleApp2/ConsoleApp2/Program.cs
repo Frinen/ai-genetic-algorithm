47bf76bbac8ab10f523b0451b965f9d98e143268
unknown <extremalbp@gmail.com>
2018-12-04 21:05:38 +0200

    its done probably

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -20,6 +20,8 @@ namespace ConsoleApp2
         List<double> fitness;
         List<double> chanses;
         List<double> fathers;
+        List<string> pearentsBinary;
+        double maxfitness;
         double sum;
         public Genetic()
         {
@@ -27,28 +29,62 @@ namespace ConsoleApp2
             chanses = new List<double>();
             fathers = new List<double>();
             population = new List<double>();
+            
             for(int i = 0; i<10;i++)
             {
                 Random rnd = new Random();
-                population.Add(rnd.Next(31));
+                population.Add(rnd.Next(2, 31));
             }
         }
 
         public void start()
         {
             sum = 0;
-            foreach(var one in population)
+            for(int i = 0; i < population.Count;i++)
             {
-                fitness.Add(func(one));
-                sum += func(one);
+                var f = func(population[i]);
+                fitness.Add(f);
+                sum += f;
+                if(i==0)
+                {
+                    maxfitness = f;
+                }
+                if(maxfitness < f)
+                {
+                    maxfitness = f;
+                }
             }
-            roulete();
-            NewPopulation();
+            Console.WriteLine($"Max fitnrss {maxfitness}");
+            double fitnessDiff=0.5;
+            
+            while(fitnessDiff>0.1)
+            {
+                var oldfit = maxfitness;
+                roulete();
+                NewPopulation();
+                maxfitness = func(population[0]);
+                fitness = new List<double>();
+                for (int i = 0; i < population.Count; i++)
+                {
+                    var f = func(population[i]);
+                    if(maxfitness<f)
+                    {
+                        maxfitness = f;
+                    }
+                    fitness.Add(f);
+                }
+                fitnessDiff = Math.Abs( oldfit - maxfitness);
+                Console.WriteLine($"Max fitnrss {maxfitness}");
+                Console.WriteLine($"Fitnrss dif {fitnessDiff} (old-) {oldfit} (new-) {maxfitness}");
+                Console.WriteLine(" ");
+            }
+            Console.ReadLine();
+            
         }
 
         double func(double x)
         {
-            return (Math.Pow(x, 2) * -1+x);
+            return (Math.Pow(x, 2) * -1+50);
         }
         void roulete()
         {
@@ -94,7 +130,7 @@ namespace ConsoleApp2
 
         void NewPopulation()
         {
-            List<string> pearentsBinary = new List<string>();
+            pearentsBinary = new List<string>();
             var maxlen = 0;
             foreach (var pearent in fathers)
             {
@@ -123,31 +159,46 @@ namespace ConsoleApp2
                 }
             }
             var he = pearentsBinary;
-            Mutation(pearentsBinary);
-            Crossover(pearentsBinary);
+            Mutation();
+            var hehe = pearentsBinary;
+            
+
+            Crossover();
         }
 
-        void Mutation(List<string> pearentsBinary)
+        void Mutation()
         {
             Random rnd = new Random();
-            foreach(var parent in pearentsBinary)
+            
+            for(int i = 0; i<pearentsBinary.Count; i++)
             {
-                if(rnd.Next(10)==8)
+                if (/*rnd.Next(10)*/ 8 == 8)
                 {
-                    int position = rnd.Next(parent.Length);
-                    if(parent[position].Equals("1"))
+                    int position = rnd.Next(pearentsBinary[i].Length);
+                    var chego = pearentsBinary[i][position];
+                    var blyat = pearentsBinary[i][position].Equals('1');
+                    if (pearentsBinary[i][position].Equals('1'))
                     {
-                        parent.Remove(position, 1).Insert(position, "0");
+                        Console.WriteLine(pearentsBinary[i]);
+                        var tmp = pearentsBinary[i].Remove(position, 1);
+                        tmp = tmp.Insert(position, "0");
+                        pearentsBinary[i] = tmp;
+                        Console.WriteLine(pearentsBinary[i]);
                     }
                     else
                     {
-                        parent.Remove(position, 1).Insert(position, "1");
+                        Console.WriteLine(pearentsBinary[i]);
+                        var tmp = pearentsBinary[i].Remove(position, 1);
+                        tmp = tmp.Insert(position, "1");
+                        pearentsBinary[i] = tmp;
+                        Console.WriteLine(pearentsBinary[i]);
                     }
+                    Console.WriteLine("");
                 }
             }
         }
 
-        void Crossover(List<string> pearentsBinary)
+        void Crossover()
         {
             Random rnd = new Random();
             List<Pair> pairs = new List<Pair>();
@@ -171,6 +222,7 @@ namespace ConsoleApp2
                 nextPopulation.Add(Convert.ToInt32(binStr, 2));
             }
             var test = nextPopulation;
+            population = nextPopulation;
         }
         class Pair
         {
